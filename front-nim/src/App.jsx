1c9f71110c7d10c0682672e579010abda3fb7450
CelineViala <celin68644@aol.com>
2023-01-23 23:09:58 +0100

    css

--- front-nim/src/App.jsx
+++ front-nim/src/App.jsx
@@ -31,20 +31,28 @@ function App() {
 
     return (
         <div className="App">
-            <h1>Jeu de Nim</h1>
-            <h2 className={`${(!gameData.waiting && gameData.boardHidden) && `${style.hidden}`}`}>{`Bienvenue ${gameData.pseudo?.toUpperCase()}`}</h2>
+            <h1 className={style.title}>Jeu de Nim</h1>
+
+            <h2 className={`${style.title} ${style.titleh2} ${(!gameData.waiting && gameData.boardHidden) ? `${style.hidden}` : ''}`}>{`Bienvenue ${gameData.pseudo?.toUpperCase()}`}</h2>
+            <p className={style.rule}>
+                Regle: Retirer de une à 3 allumettes.
+                Celui qui prend la dernière allumette disponible gagne.
+
+            </p>
             <Game />
             <div className={`${!gameData.boardHidden && `${style.hidden}`}`}>
-                <label className={`${(!gameData.boardHidden || gameData.waiting) && `${style.hidden}`}`}>Pseudo :</label>
-                <input
-                    className={`${(!gameData.boardHidden || gameData.waiting) && `${style.hidden}`}`}
-                    ref={inputRef}
-                    placeholder="Entre un pseudo"
-                    onChange={(event) => {
-                        setPseudo(event.target.value);
-                    }}
-                />
-                <p>{gameData.message}</p>
+                <div className={style.form}>
+                    <label className={`${style.labelPseudo} ${(!gameData.boardHidden || gameData.waiting) && `${style.hidden}`}`}>Pseudo :</label>
+                    <input
+                        className={`${style.inputPseudo}  ${(!gameData.boardHidden || gameData.waiting) && `${style.hidden}`}`}
+                        ref={inputRef}
+                        placeholder="Entre un pseudo"
+                        onChange={(event) => {
+                            setPseudo(event.target.value);
+                        }}
+                    />
+                </div>
+                <p className={style.message}>{gameData.message}</p>
                 <button type="button" className={`${(!gameData.boardHidden || gameData.waiting) && `${style.hidden}`}`} onClick={handlePlay}>JOUER</button>
             </div>
         </div>
--- front-nim/src/Game.jsx
+++ front-nim/src/Game.jsx
@@ -6,6 +6,7 @@ import Match from './Match';
 import { DataGameContext } from './contexts/DataGame';
 import useSocket from './hooks/useSocket';
 import useGame from './hooks/useGame';
+import volumePict from './media/volume.png';
 
 function Game() {
     const {
@@ -48,14 +49,15 @@ function Game() {
     }, [gameData.socket, gameData.currentPlayer]);
     return (
         <div className={`${style.game} ${gameData.boardHidden && `${style.hidden}`}`}>
-            <h2>
-                {`Adversaire : ${(gameData.adv?.pseudo?.toUpperCase())}`}
+            <h2 className={style.adv}>
+                {`Votre adversaire : ${(gameData.adv?.pseudo?.toUpperCase())}`}
             </h2>
-            <h3>{gameData.message}</h3>
+            <h3 className={style.message}>{gameData.message}</h3>
+            <img src={volumePict} alt="volume" width={30} />
             <button className={`${gameData.nbMatch === 10 && `${style.hidden}`}`} onClick={handleValid} type="button">VALIDER</button>
-            <button className={`${gameData.nbMatch === 10 && `${style.hidden}`}`} onClick={emitStopGame} type="button">ARRETER</button>
 
             <button className={`${gameData.nbMatch < 10 && `${style.hidden}`}`} onClick={emitRestartGame} type="button">RETOUR</button>
+
             <div className={style['matches-box']}>
                 {Array.from(Array(10).keys(), (x) => (
                     <Match
@@ -64,6 +66,7 @@ function Game() {
                     />
                 ))}
             </div>
+            <button className={`${style.stopGame} ${gameData.nbMatch === 10 && `${style.hidden}`}`} onClick={emitStopGame} type="button">ARRETER LA PARTIE</button>
 
         </div>
 
